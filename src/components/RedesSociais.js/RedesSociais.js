f52d2250eaf4a67ff8c4f1d2fbb2fe1998ddefcc
Filipe Barros <filipedebarros@hotmail.com>
2023-09-21 17:41:19 -0300

    O link do zapzap tava dando pro insta, agora zap

--- src/components/RedesSociais.js/RedesSociais.js
+++ src/components/RedesSociais.js/RedesSociais.js
@@ -7,7 +7,7 @@ const RedesSociais = () => {
         { id: 2, urlNome: 'Facebook', url:'https://www.facebook.com/felepsbarros/' },
         { id: 3, urlNome: 'Twitter', url:'https://twitter.com/FilepsTB'},
         { id: 4, urlNome: 'Youtube', url:'https://www.youtube.com/channel/UCWQyUuFjZJeloTGB7EsSePQ' },
-        { id: 5, urlNome: 'WhatsApp', url:'https://www.instagram.com/tb_filipe/' },
+        { id: 5, urlNome: 'WhatsApp', url:'https://api.whatsapp.com/send?phone=5551997879494&text=Eu%20vim%20aqui%20pelo%20vercel%20uhul' },
 
     ]
 
